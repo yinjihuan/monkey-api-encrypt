5449bcc240238df31cb9c79b40886fda36529471
尹吉欢 <yinjihuan@fangjia.com>
2018-05-28 17:43:55 +0800

    增加注释

--- spring-boot-starter-encrypt/src/main/java/com/cxytiandi/encrypt/auto/EncryptAutoConfiguration.java
+++ spring-boot-starter-encrypt/src/main/java/com/cxytiandi/encrypt/auto/EncryptAutoConfiguration.java
@@ -6,17 +6,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import com.cxytiandi.encrypt.advice.EncryptRequestBodyAdvice;
 import com.cxytiandi.encrypt.advice.EncryptResponseBodyAdvice;
 
+/**
+ * 加解密自动配置
+ * 
+ * @author yinjihuan
+ * 
+ * @about http://cxytiandi.com/about
+ *
+ */
 @Configuration
 @Component
 @EnableAutoConfiguration
 @EnableConfigurationProperties(EncryptProperties.class)
 public class EncryptAutoConfiguration {
 
+	/**
+	 * 配置请求解密
+	 * @return
+	 */
 	@Bean
 	public EncryptResponseBodyAdvice encryptResponseBodyAdvice() {
 		return new EncryptResponseBodyAdvice();
+	}
+	
+	/**
+	 * 配置请求加密
+	 * @return
+	 */
+	@Bean
+	public EncryptRequestBodyAdvice encryptRequestBodyAdvice() {
+		return new EncryptRequestBodyAdvice();
 	}
 	
 }
